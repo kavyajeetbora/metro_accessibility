1da8cf1bb5bc6cf0a7925a8e4826b6284bfbc27b
Kayajeet Bora <kavyajeet.bora@burohappold.com>
2024-08-30 13:01:50 +0530

    Add - folder separator

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 # Read the HTML file
-with open(r"Images\Delhi Accessibility.html", "r") as file:
+with open(r"Images/Delhi Accessibility.html", "r") as file:
     html_string = file.read()
 
 # Render the HTML content
